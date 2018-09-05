dcc0c787db54dc5a9d303ba8d027a4a417e4e97f
Bassel Mahmoud <Bassel_mac@hotmail.com>
2018-09-05 16:08:46 +0200

    DWA: init dwa planner
    Signed-off-by: Bassel Mahmoud <Bassel_mac@hotmail.com>

--- tuw_multi_robot_dwa/src/tuw_multi_robot_dwa_node.cpp
+++ tuw_multi_robot_dwa/src/tuw_multi_robot_dwa_node.cpp
@@ -15,7 +15,34 @@ int main(int argc, char** argv)
     {
         ros::init(argc, argv, argv[1]);
 
+        ros::NodeHandle nh;
+
+        //TODO: check if rate is enough
+        ros::Rate r(20);
+
+        //init DWAPlannerROS
+        tf::TransformListener tf(ros::Duration(10));
+
+        // TODO: subscribe to the global planner
+        //TODO: add name of the cost map every robot will need a cost map
+        costmap_2d::Costmap2DROS costmap("my_costmap", tf);
+
+        dwa_local_planner::DWAPlannerROS dp;
+
+        //TODO: add unique name for the dwa since this will be used in multirobot environment
+        dp.initialize("my_dwa_planner", &tf, &costmap);
+
+        while(ros::ok())
+        {
+            ros::spinOnce();
+            r.sleep();
+        }
+
+        return 0;
     } else {
         ROS_INFO("Please specifie name \nrosrun tuw_multi_robot_dwa tuw_multi_robot_dwa_node [name]");
+        return 1;
     }
+
+
 }
